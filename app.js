0b96d45a8011afa8566f71007af0bae3d4a0b74a
Ignacio Cholaquides <nacho@MacBook-Air-de-Ignacio.local>
2023-02-15 17:35:51 -0300

    Minor changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express ();
+const port = process.env.PORT || 3000;
 
 const newTasks = [];
 const newTasksWork = [];
@@ -62,6 +63,6 @@ app.post("/", (req, res) => {
     
 })
 
-app.listen(3000, () => {
-    console.log("Listening on Port 3000");
+app.listen(port, () => {
+    console.log("Listening on Port: " + port);
 })
